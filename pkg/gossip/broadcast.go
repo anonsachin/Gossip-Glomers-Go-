430e2cde447f55d4ab5c5e7cb00c62fc8ccd71f1
anonsachin <sachin94roxz@gmail.com>
2023-10-12 22:24:19 +0530

    Broadcast now handles async rpc, that can retry on error.

--- pkg/gossip/broadcast.go
+++ pkg/gossip/broadcast.go
@@ -1,12 +1,14 @@
 package gossip
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"os"
 	"reflect"
 	"strconv"
 	"sync"
+	"time"
 
 	"log"
 
@@ -96,13 +98,83 @@ func (g *Gossip) Broadcast(msg maelstrom.Message) error {
 	// send message to the other nodes
 	//////////////////////////////////////////
 	nodes := make(map[string]bool)
+	respChan := make(chan ReturnMessage)
+	retryLimit := 10
 	for k := range g.topology{
 		
-		g.Node.Send(k,body)
-		nodes[k] = true
+		if err := g.Node.RPC(k,body,g.RetryBroadCastHandler(respChan, 0, retryLimit)); err != nil {
+			return err
+		}
+		nodes[k] = false
 	}
+	ctx , _ := context.WithTimeout(context.Background(), 5*time.Second)
+	go g.RetryResponseHandler(respChan, ctx,nodes)
 
 	return g.Node.Reply(msg, response)
+}
+
+type ReturnMessage struct{
+	Message maelstrom.Message
+	RetryCount int
+	RetryLimit int
+}
+
+func (g *Gossip) RetryBroadCastHandler(respChan chan <- ReturnMessage, retryCount int, retryLimit int) maelstrom.HandlerFunc {
+	return func(msg maelstrom.Message) error {
+		response := ReturnMessage{
+			Message: msg,
+			RetryCount: retryCount,
+			RetryLimit: retryLimit,
+		}
+		respChan <- response
+		return nil
+	}
+}
+
+func (g *Gossip) RetryResponseHandler(respChan chan ReturnMessage, ctx context.Context, nodes map[string]bool) {
+	for {
+		select{
+		case <- ctx.Done():{
+			g.log.Println("Response handler timed out!")
+			return
+		}
+		case r := <- respChan: {
+			var body map[string]any
+			var rpcErr maelstrom.RPCError
+			if rpcErr := r.Message.RPCError(); rpcErr == nil {
+				g.log.Printf("The %s source recieved message successfully.", r.Message.Src)
+				nodes[r.Message.Src] = true
+				break
+			}
+			// If there is an error with the bad message body retry will not take place.
+			if rpcErr.Code == 13 {
+				g.log.Printf("The returned message, caused an error from %s => %#v",r.Message.Src, rpcErr.Text)
+				nodes[r.Message.Src] = true
+				break
+			}
+			// Im currently discarding bad responses
+			if r.RetryCount < r.RetryLimit {
+				g.log.Printf("Retrying to node %s from %s",r.Message.Src,g.Node.ID())
+				_ = json.Unmarshal(r.Message.Body, &body)
+				go func(body map[string]any, src string, respChan chan <- ReturnMessage, retryCount int, retryLimit int){
+					time.Sleep(100*time.Millisecond)
+					g.Node.RPC(src,body,g.RetryBroadCastHandler(respChan, retryCount, retryLimit))
+				}(body, r.Message.Src, respChan, (r.RetryCount + 1), r.RetryLimit)
+			}
+		}
+		}
+		g.log.Printf("The node %s status of broadcast %#v",g.Node.ID(),nodes)
+		overAllStatus := true
+		for _, status :=  range nodes {
+			overAllStatus = overAllStatus && status
+		}
+
+		if overAllStatus {
+			g.log.Printf("All the messages were sent %#v",nodes)
+			return
+		}
+
+	}
 }
 
 func (g *Gossip) Read(msg maelstrom.Message) error {
